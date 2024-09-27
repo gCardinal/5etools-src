5eba88f7a86559a512416efad25f6d6e6ddfe54e
Guyllaume Cardinal <guyllaume.cardinal@gmail.com>
2024-09-27 17:00:28 -0400

    remove images before update

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/5etools-mirror-3/5etools-img.git 5etools-src/img
+rm -rf img && git clone https://github.com/5etools-mirror-3/5etools-img.git img
 ls -la
-npm i
 npm run build:sw:prod
 ls -la
