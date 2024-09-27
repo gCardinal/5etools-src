7a2d6850fe7f0f1711745f083b167f0b81498d58
Guyllaume Cardinal <guyllaume.cardinal@gmail.com>
2024-09-27 17:02:51 -0400

    reuse cached image if they exist

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,10 @@
-rm -rf img && git clone https://github.com/5etools-mirror-3/5etools-img.git img
+if [ ! -d "img" ]; then
+  git clone https://github.com/5etools-mirror-3/5etools-img.git img
+else
+  cd img || exit
+  git pull
+  cd ..
+fi
 ls -la
 npm run build:sw:prod
 ls -la
